787e737eff5fe4fba0740bab540edc7addccf535
DUBREZ Francois <francois.dubrez@gmail.com>
2022-10-12 22:33:31 +0200

    ✨ 1st commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# soundboard
+
+## Getting started
+
+```
+npm run start
+```
+
+Server start on port `8000` by default
+
+You can override server port setting `PORT` environment variable
+
+## License
+
+Copyright © 2022 François Dubrez
+This work is free. You can redistribute it and/or modify it under the
+terms of the [Do What The Fuck You Want To Public License](http://www.wtfpl.net/), Version 2,
+as published by Sam Hocevar. See the [LICENSE](LICENSE) file for more details.
+
+![WTFPL](http://www.wtfpl.net/wp-content/uploads/2012/12/logo-220x1601.png)
---
+++ index.mjs
@@ -0,0 +1,91 @@
+import * as fs from 'node:fs';
+import * as http from 'node:http';
+import * as path from 'node:path';
+import { Readable } from 'node:stream';
+
+
+const PORT = 8000;
+
+const MIME_TYPES = {
+    default: 'application/octet-stream',
+    html: 'text/html; charset=UTF-8',
+    js: 'application/javascript; charset=UTF-8',
+    css: 'text/css',
+    png: 'image/png',
+    jpg: 'image/jpg',
+    gif: 'image/gif',
+    ico: 'image/x-icon',
+    svg: 'image/svg+xml',
+    mp3: 'audio/mpeg',
+    json: 'applicaton/json'
+};
+
+const STATIC_PATH = path.join(process.cwd(), './static');
+const sounds = fs.readdirSync(path.join(process.cwd(), './static/medias/music'))
+    .map(filename => {
+        const split = filename.split(".mp3")[0].split("-");
+        return {
+            theme: split[0].replace(/_/g, " "),
+            title: split[1].replace(/_/g, " "),
+            filename
+        }
+    })
+
+const toBool = [() => true, () => false];
+
+const buildIndex = (sounds) => {
+    return `<!DOCTYPE html>
+<head>
+</head>
+<body>
+    ${sounds.map(({theme, title, filename}) => `<div>
+<h3>${theme} - ${title}</h3>
+<audio controls>
+<source src="/medias/music/${filename}" />
+</audio>
+</div>`).join("\n")}
+</body>
+</html>`
+}
+
+const playSound = ({theme, title, filename}) => {
+    // todo
+}
+
+const prepareFile = async (url) => {
+    const paths = [STATIC_PATH, url];
+    if (url.startsWith('/play')) {
+        playSound(sounds[0]);
+        const stream = Readable()
+        stream.push(JSON.stringify({status: "OK"}))
+        stream.push(null) 
+        return {found: true, ext: "json", stream}
+    }
+    if (url.endsWith('/')) {
+        const response = buildIndex(sounds);
+        console.log(response)
+        const stream = Readable()
+        stream.push(response)
+        stream.push(null) 
+        return {found: true, ext: "html", stream};
+    }
+    const filePath = path.join(...paths);
+    const pathTraversal = !filePath.startsWith(STATIC_PATH);
+    const exists = await fs.promises.access(filePath).then(...toBool);
+    const found = !pathTraversal && exists;
+    const streamPath = found ? filePath : STATIC_PATH + '/404.html';
+    const ext = path.extname(streamPath).substring(1).toLowerCase();
+    const stream = fs.createReadStream(streamPath);
+    return { found, ext, stream };
+};
+
+http.createServer(async (req, res) => {
+    const file = await prepareFile(req.url);
+    const statusCode = file.found ? 200 : 404;
+    const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
+    res.writeHead(statusCode, { 'Content-Type': mimeType });
+    file.stream.pipe(res);
+    console.log(`${req.method} ${req.url} ${statusCode}`);
+}).listen(PORT);
+
+console.log(`Server running at http://127.0.0.1:${process.env.PORT || PORT}/`);
